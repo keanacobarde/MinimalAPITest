ab3f1b834cc98676f3e277f92d7c61f6b11f1ec6
Keana Cobarde <keanacobarde@gmail.com>
2024-01-13 13:45:20 -0600

    Add project files.

---
+++ MinimalAPITest/TodoDb.cs
@@ -0,0 +1,13 @@
+﻿namespace MinimalAPITest
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+
+    class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
